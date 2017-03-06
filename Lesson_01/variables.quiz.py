ec45de9b7b1b0779b128527391b3933296d30ce6
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2017-03-06 22:46:06 +0200

    variable assigment

--- Lesson_01/variables.quiz.py
+++ Lesson_01/variables.quiz.py
@@ -6,9 +6,15 @@
 # speed_of_light in meters per second
 # cycles_per_second is 2.7 GHz
 
-speed_of_light = 299792458.0
-cycle_per_second = 2700000000.0
+speed_of_light = 299792458.0 # meters per second
+cycle_per_second = 2700000000.0 # processor speed 2.7Ghz
 
-meters = speed_of_light / cycle_per_second
+#meters = speed_of_light / cycle_per_second
+cycle_distance = speed_of_light / cycle_per_second
 
-print meters
+print cycle_distance
+
+cycle_per_second = 2800000000.0 # upgraded our processor 2.8Ghz
+cycle_distance = speed_of_light / cycle_per_second
+
+print cycle_distance
