e6cf920a3e5c65fc75aebbc808bcf6be05645477
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2017-03-06 22:37:17 +0200

    variables quiz: speed of light

---
+++ Lesson_01/variables.quiz.py
@@ -0,0 +1,14 @@
+
+# Given the variables defined here, write Python
+# code that prints out the distance, in meters,
+# that light travels in one processor cycle.
+
+# speed_of_light in meters per second
+# cycles_per_second is 2.7 GHz
+
+speed_of_light = 299792458.0
+cycle_per_second = 2700000000.0
+
+meters = speed_of_light / cycle_per_second
+
+print meters
