e38d3ed11e82d839a6d5f662f0adfc5e71ea2dfd
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2017-03-07 22:30:52 +0200

    finding strings in strings

---
+++ Lesson_01/strings.finding.strings.py
@@ -0,0 +1,9 @@
+
+pythagoras = 'There is geometry in the humming of the strings, there is music in the spacing of the spheres.'
+print pythagoras.find('string');
+print pythagoras[40:]
+print pythagoras[10:]
+print pythagoras.find('T')
+print pythagoras.find('sphere')
+print pythagoras[86:]
+print pythagoras.find('algebra')
