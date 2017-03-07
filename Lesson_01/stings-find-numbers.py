61d00c7e2cac86573f2b66bb3c4c48dc27a1718f
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2017-03-07 22:58:12 +0200

    finding with numbers

--- Lesson_01/stings-find-numbers.py
+++
@@ -1,2 +0,0 @@
-
-danton = "De l'audace, encore de l'audace, et toujours de l'audace"
---
+++ Lesson_01/strings-find-numbers.py
@@ -0,0 +1,11 @@
+
+danton = "De l'audace, encore de l'audace, et toujours de l'audace"
+print danton.find('audace')
+print danton.find('audace', 0)
+print danton.find('audac', 5)
+print danton.find('audace', 6)
+print danton[6:]
+print danton[25:]
+print danton[26:]
+print danton[47:]
+print danton.find('audace', 48)
