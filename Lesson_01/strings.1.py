04625fbc550386b5800c4dd411c1434678b59a53
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2017-03-07 22:00:46 +0200

    get index of a string

--- Lesson_01/strings.1.py
+++ Lesson_01/strings.1.py
@@ -1,5 +1,4 @@
 # Define a variable, name, and assign to it a string that is your name.
 
 name = 'mannuel ferreira'
-
-print 'Hello ' + name
+print 'Hello ' + name + '!' * 10
---
+++ Lesson_01/strings.indexing.py
@@ -0,0 +1,4 @@
+# Get the index of a string
+
+name = 'Manny'
+print name[-2];
