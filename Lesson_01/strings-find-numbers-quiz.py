2edd1a0dbee489a0e7a974f94a4481c9173ba99c
manidf <mannuel.ferreira@gmail.com>
2017-03-08 17:28:26 +0200

    finding with numbers quiz

--- Lesson_01/strings-find-numbers-quiz.py
+++ Lesson_01/strings-find-numbers-quiz.py
@@ -1,3 +1,10 @@
-s = ''
-t = ''
-i = ''
+s = 'udacity'
+t = 'city'
+i = 3
+
+# find always retruns -1 when it cant find character
+
+print s[i:].find(t)      # 0
+# print s.find(t)[:i]      # fail, there is no way to select a subsequnce of characters form a number
+print s[i:].find(t) + i
+print s[i:].find(t[i:])
