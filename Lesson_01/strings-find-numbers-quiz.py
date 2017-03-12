f06a553a17a4d919460fda66a3b06833088e47df
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2017-03-12 21:30:26 +0200

    quiz 8

--- Lesson_01/strings-find-numbers-quiz.py
+++ Lesson_01/strings-find-numbers-quiz.py
@@ -5,6 +5,6 @@ i = 3
 # find always retruns -1 when it cant find character
 
 print s[i:].find(t)      # 0
-# print s.find(t)[:i]      # fail, there is no way to select a subsequnce of characters form a number
+# print s.find(t)[:i]      # fail, there is no way to select a subsequence of characters form a number
 print s[i:].find(t) + i
 print s[i:].find(t[i:])
