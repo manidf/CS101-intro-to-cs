c706d1da6573b8fe12bc907e23f2a0fa82428226
manidf <mannuel.ferreira@gmail.com>
2017-03-23 22:37:17 +0200

    train focus quiz

---
+++ Lesson_01/quiz-train-focus.py
@@ -0,0 +1,9 @@
+# check all that will print Udacity
+s = "CidatyUcityda"
+
+# print s[6] + s[-2:] + s[7:12];
+# print s[6] + s[-2:] + s[7:11]; # yes
+# print s[6] + s[2:4] + s[7:13];
+# print s[-7] + s[2:4] + s[7:11]; # yes
+# print s[6] + s[-2] + s[3] + s[:2] + s[4:6]; # yes
+print s[6] + s[2] + s[3] + s[7:11];
